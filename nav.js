968b635b663c01dbadb3242a0671ee101e15c35b
Gary Doran, Jr <garydoranjr@gmail.com>
2011-01-18 20:23:10 -0500

    Initial commit

---
+++ nav.js
@@ -0,0 +1,17 @@
+function nav(id) {
+    var NAVS = [['index.html', 'Home'],
+                ['https://github.com/cwru-eecs338', 'Code'],
+                ['intro.html', 'Intro to Linux'],
+                ['resources.html', 'Resources']
+               ];
+    var navElement = document.getElementById(id);
+    for(var i = 0; i < NAVS.length; i++) {
+        var p = document.createElement('p');
+        var a = document.createElement('a');
+	var t = document.createTextNode(NAVS[i][1]);
+	a.setAttribute('href', NAVS[i][0]);
+	a.appendChild(t);
+        p.appendChild(a);
+        navElement.appendChild(p)
+    }
+}
